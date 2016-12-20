0ae32acbc378770b6e101739597d2ee348847dd5
jonathan6169 <jonathan.albarracin.16@gmail.com>
2016-12-20 14:44:52 -0500

    Add files via upload

---
+++ practice.py
@@ -0,0 +1,13 @@
+print(100*1.1*1.1*1.1*1.1*1.1*1.1*1.1)
+savings = 100
+factor = 1.10
+print(savings*factor**7)
+result = 194.87171000000015
+desc = "compound interest"
+profitable = True
+y = False
+print(savings)
+print(factor)
+print(desc)
+print(profitable)
+print("I started with $100 and now I have $194.87171000000015. Great!")
