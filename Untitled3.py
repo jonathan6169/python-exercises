9f872d9319568daab35e92bce32970863c4d28c4
jonathan6169 <jonathan.albarracin.16@gmail.com>
2016-12-22 15:04:03 -0500

    Add files via upload

---
+++ Untitled3.py
@@ -0,0 +1,33 @@
+print("Hello", "World", "!")
+#This prints Hello World !, has spaces bewtween all the words and symbols
+
+#print("Hello", "World!)
+#The compile-time error is that there is no " at the end of the string
+
+#Write three versions of the hello.py program that have different compile-time
+#errors.Write a version that has a run-time error.
+
+#prnt("hello")
+#print(1/0)
+#print("hello)
+#print("hello word") (the run-time error is that world is mispelled
+
+#compile-time errors are syntax mistakes meaning that the computer can't process
+#them
+#run-time error is a mistake that the computer processes, but something is
+#mispelled or the wrong value is plugged in
+
+print("hola como estas")
+
+print(1+2+3+4+5+6+7+8+9+10)
+
+print(1*2*3*4*5*6*7*8*9*10)
+
+x = 3
+print(1000*1.05**x)
+
+print(' *****   ***   *     *      *       *****      *     *       *     *    *')
+print('   *    *   *  * *   *     * *        *')
+print('   *    *   *  *  *  *    *   *       *')
+print('*  *    *   *  *   * *   * * * *      *')
+print(' **      ***   *     *  *       *     *')
