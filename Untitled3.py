ceec2f244944c5d48efd7356f1a415c47aff5180
jonathan6169 <jonathan.albarracin.16@gmail.com>
2017-01-22 21:36:06 -0500

    Update Untitled3.py

--- Untitled3.py
+++ Untitled3.py
@@ -1,33 +1,69 @@
+
+#1 
+What does this program print? Pay close attention to spaces.
 print("Hello", "World", "!")
-#This prints Hello World !, has spaces bewtween all the words and symbols
+This prints Hello World !
 
-#print("Hello", "World!)
-#The compile-time error is that there is no " at the end of the string
+#2
+What is the compile-time error in this program?
+print("Hello", "World!)
+The compile-time error is that "World! is missing the last ".
 
-#Write three versions of the hello.py program that have different compile-time
-#errors.Write a version that has a run-time error.
+#3
+Write three versions of the hello.py program that have different compile-time errors.
+Write a version that has a run-time error.
+print"Hello world!")
+prnt("Hello World!")
+print(Hello World !)
 
-#prnt("hello")
-#print(1/0)
-#print("hello)
-#print("hello word") (the run-time error is that world is mispelled
+print("Helo world!")
 
-#compile-time errors are syntax mistakes meaning that the computer can't process
-#them
-#run-time error is a mistake that the computer processes, but something is
-#mispelled or the wrong value is plugged in
+#4
+How do you discover compile-time errors? How do you discover run-time errors?
+Compile time errors are syntax errors so they're easily found since the prevent the
+program from running.
+Run time errors are harder to find, to locate these you have to see what the program
+outputs and make sure it's all correct.
 
-print("hola como estas")
+#5
+Write a program that prints a greeting of your choice, perhaps in a language other
+than English.
+print("Hola Como Estas")
 
+#6
+Write a program that prints the sum of the first ten positive integers, 1 + 2 + … + 10.
 print(1+2+3+4+5+6+7+8+9+10)
 
+#7
+Write a program that prints the product of the first ten positive integers, 1 × 2 × … × 10. (Use * to indicate multiplication in Python.)
 print(1*2*3*4*5*6*7*8*9*10)
 
+#8
+ Write a program that prints the balance of an account after the first, second, and
+third year. The account has an initial balance of $1,000 and earns 5 percent interest
+per year.
 x = 3
 print(1000*1.05**x)
 
-print(' *****   ***   *     *      *       *****      *     *       *     *    *')
-print('   *    *   *  * *   *     * *        *')
-print('   *    *   *  *  *  *    *   *       *')
-print('*  *    *   *  *   * *   * * * *      *')
-print(' **      ***   *     *  *       *     *')
+#9 
+
+
+print(" ___________")
+print("|           |")
+print("| Jonathan  |")
+print("|___________|")
+
+# 10 
+
+print(' *****   ***   *     *      *    *****  *   *      *     *     *')
+print('   *    *   *  * *   *     * *     *    *   *     * *    * *   *')
+print('   *    *   *  *  *  *    *   *    *    *****    *   *   *  *  *')
+print('*  *    *   *  *   * *   * * * *   *    *   *   * * * *  *   * *')
+print(' **      ***   *     *  *       *  *    *   *  *       * *     *')
+
+# 11
+print("  _||||___")
+print(" (|(*) (*)|)")
+print("  |   ^   |")
+print("  | \___/ |")
+print("  |_______|")
